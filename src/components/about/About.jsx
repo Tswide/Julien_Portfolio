1f653198be4b35df5304179d1383565adb9a09ae
Tswide <julienjcbs@protonmail.com>
2023-11-07 12:45:35 +0100

    maj infos

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import "./about.css"
 import AboutImg from "../../assets/profile.jpg"
-import CV from "../../assets/RechercheAlternance_Developpeur_WEB"
+import CV from "../../assets/cv.pdf"
 import Info from './Info'
 
 const About = () => {
